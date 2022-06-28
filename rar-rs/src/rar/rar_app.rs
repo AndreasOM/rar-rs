06a66ce8930bd3e3ef336f1c3210a7a33b506844
Andreas Neukoetter <andreas@omni-mad.com>
2022-06-28 19:58:27 +0200

    + Hook up filesystem. + Load text from file, and log it.

--- rar-rs/src/rar/rar_app.rs
+++ rar-rs/src/rar/rar_app.rs
@@ -1,19 +1,62 @@
+use oml_game::system::filesystem_disk::FilesystemDisk;
+use oml_game::system::filesystem_layered::FilesystemLayered;
+use oml_game::system::System;
 use oml_game::window::{Window, WindowUpdateContext};
 use oml_game::App;
 
 pub struct RarApp {
+	system:  System,
 	is_done: bool,
 }
 
 impl RarApp {
 	pub fn new() -> Self {
-		Self { is_done: false }
+		Self {
+			system:  System::new(),
+			is_done: false,
+		}
+	}
+	// :TODO: Consider moving this into game package
+	fn add_filesystem_disk(&mut self, lfs: &mut FilesystemLayered, path: &str, enable_write: bool) {
+		let datadir = if path.starts_with("/") {
+			path.to_owned()
+		} else {
+			let cwd = std::env::current_dir().unwrap();
+			let cwd = cwd.to_string_lossy();
+
+			let datadir = format!("{}/{}", &cwd, &path);
+			datadir
+		};
+
+		let mut dfs = FilesystemDisk::new(&datadir);
+		if enable_write {
+			dfs.enable_write();
+		}
+
+		lfs.add_filesystem(Box::new(dfs));
 	}
 }
 
 impl App for RarApp {
 	fn setup(&mut self, window: &mut Window) -> anyhow::Result<()> {
 		window.set_title("RAR - RS");
+
+		let mut lfs = FilesystemLayered::new();
+		self.add_filesystem_disk(&mut lfs, "../rar-data", false);
+
+		println!("lfs: {:?}", &lfs);
+
+		self.system.set_default_filesystem(Box::new(lfs));
+
+
+		let mut something_file = self.system.default_filesystem_mut().open( "something.txt" );
+//		println!("sf: {:?}", &something_file);
+//		println!("valid?: {:?}", something_file.is_valid());
+//		println!("size: {:?}", something_file.size());
+		let something = something_file.read_as_string();
+
+		println!("Something: {}", &something);
+
 		Ok(())
 	}
 	fn teardown(&mut self) {}
