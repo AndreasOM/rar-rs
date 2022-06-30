5f530c8c7f61d017487fdef02221a5804d563561
Andreas Neukoetter <andreas@omni-mad.com>
2022-06-30 18:35:08 +0200

    : [episode-0002] Add entities based on Fiiish-rs! : [episode-0002] Ignore target folder.

--- rar-rs/src/rar.rs
+++ rar-rs/src/rar.rs
@@ -2,3 +2,6 @@ pub mod rar_app;
 pub use rar_app::RarApp;
 pub mod effect_ids;
 pub mod layer_ids;
+pub mod entities;
+mod entity_update_context;
+	pub use entity_update_context::EntityUpdateContext;
