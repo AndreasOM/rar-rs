13c202162751f655f9f834bbd3ace76607746719
Andreas Neukoetter <andreas@omni-mad.com>
2022-06-30 18:35:08 +0200

    : [episode-0002] Add entities based on Fiiish-rs! : [episode-0002] Ignore target folder.

---
+++ rar-rs/src/rar/entities.rs
@@ -0,0 +1,28 @@
+mod entity;
+	pub use entity::Entity as Entity;
+	pub use entity::EntityData as EntityData;
+	pub use entity::EntityState as EntityState;
+mod entity_configuration;
+	pub use entity_configuration::AnimatedTextureConfiguration as AnimatedTextureConfiguration;
+	pub use entity_configuration::EntityConfiguration as EntityConfiguration;
+	pub use entity_configuration::EntityConfigurationManager as EntityConfigurationManager;
+mod entity_ids;
+	pub use entity_ids::EntityId as EntityId;
+mod entity_types;
+	pub use entity_types::EntityType as EntityType;
+	/*
+mod entity_manager;
+	pub use entity_manager::EntityManager as EntityManager;
+	*/
+/*
+mod background;
+	pub use background::Background as Background;
+mod coin;
+	pub use coin::Coin as Coin;
+mod obstacle;
+	pub use obstacle::Obstacle as Obstacle;
+mod fish;
+	pub use fish::Fish as Fish;
+*/
+mod player;
+	pub use player::Player;
