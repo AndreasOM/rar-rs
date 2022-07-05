3c625d724ffd99965f7bd384bcbefe9ca49fa9c0
Andreas Neukoetter <andreas@omni-mad.com>
2022-07-05 17:33:35 +0200

    + [off stream] Render all player actions from both sides (stereoscopy) and NLA. + [off stream] Make background of player transparent.

--- rar-workfiles/entities/player/update_animations.sh
+++ rar-workfiles/entities/player/update_animations.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 
-for i in idle_0_right/????.png
-do
-	n=$(basename $i .png)
-	n=$(echo ${n} | sed 's/^0*//')
-	n=$((${n} -1))
-	n=$(printf "%04d" ${n})
-	o="../../../rar-content/entities/player/player-idle-right-${n}.png"
-	echo ${i} "->" ${o}
-	cp ${i} ${o}
-done
+copy_action() {
+	prefix=$1
+	suffix=$2
+	first_frame=$3
+	last_frame=$4
+	output=$5
 
-for i in idle_0_left/????.png
-do
-	n=$(basename $i .png)
-	n=$(echo ${n} | sed 's/^0*//')
-	n=$((${n} -1))
-	n=$(printf "%04d" ${n})
-	o="../../../rar-content/entities/player/player-idle-left-${n}.png"
-	echo ${i} "->" ${o}
-	cp ${i} ${o}
-done
+	echo $1 $2 $3 $4 $5
+	for i in ${prefix}/????${suffix}.png
+	do
+		# echo $i
+		n=$(basename $i ${suffix}.png)
+		if [ ${n} == "0000" ]
+		then
+			n=0
+		else
+			n=$(echo ${n} | sed 's/^0*//')
+		fi
+		# echo "? ${first_frame} <= ${n} <= ${last_frame}"
+		if [ ${n} -ge ${first_frame} ] && [ ${n} -le ${last_frame} ]
+		then
+			n=$((${n} -${first_frame}))
+			n=$(printf "%04d" ${n})
+			# echo ${n}
+			o="../../../rar-content/entities/player/${output}-${n}.png"
+			echo ${i} "->" ${o}
+			cp ${i} ${o}
+		fi
+		# echo ${n}
+	done
+
+}
+
+copy_action "combined" "_ToRight" 0 8 player-idle-right
+copy_action "combined" "_ToLeft" 0 8 player-idle-left
 
